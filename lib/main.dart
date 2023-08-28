2bdfc2a8f82ecb8eac86191881324d6677ab72a1
laizelin <1312242938@qq.com>
2023-08-28 10:53:25 +0800

    修改了播放源

--- lib/main.dart
+++ lib/main.dart
@@ -18,8 +18,9 @@ class MyApp extends StatelessWidget {
 
 class VideoPage extends StatelessWidget {
   final String videoUrl =
-      //'http://v.jstv.com/a/20230815/1692063007188.shtml';
-      'https://cn-sxxa-cu-02-02.bilivideo.com/upgcxcode/85/64/1247886485/1247886485-1-16.mp4?e=ig8euxZM2rNcNbRVhwdVhwdlhWdVhwdVhoNvNC8BqJIzNbfq9rVEuxTEnE8L5F6VnEsSTx0vkX8fqJeYTj_lta53NCM=&uipk=5&nbs=1&deadline=1693156021&gen=playurlv2&os=bcache&oi=3704020481&trid=0000b26dccdf12744f6595bf84381464d150h&mid=0&platform=html5&upsig=ba1c80a6baca7908fd58211a403eee19&uparams=e,uipk,nbs,deadline,gen,os,oi,trid,mid,platform&cdnid=86604&bvc=vod&nettype=0&f=h_0_0&bw=51611&logo=80000000';
+      'https://sample-videos.com/video123/flv/240/big_buck_bunny_240p_10mb.flv';
+  //'http://v.jstv.com/a/20230815/1692063007188.shtml';
+  //'https://cn-sxxa-cu-02-02.bilivideo.com/upgcxcode/85/64/1247886485/1247886485-1-16.mp4?e=ig8euxZM2rNcNbRVhwdVhwdlhWdVhwdVhoNvNC8BqJIzNbfq9rVEuxTEnE8L5F6VnEsSTx0vkX8fqJeYTj_lta53NCM=&uipk=5&nbs=1&deadline=1693156021&gen=playurlv2&os=bcache&oi=3704020481&trid=0000b26dccdf12744f6595bf84381464d150h&mid=0&platform=html5&upsig=ba1c80a6baca7908fd58211a403eee19&uparams=e,uipk,nbs,deadline,gen,os,oi,trid,mid,platform&cdnid=86604&bvc=vod&nettype=0&f=h_0_0&bw=51611&logo=80000000';
   //'https://cn-sxxa-cu-02-02.bilivideo.com/upgcxcode/85/64/1247886485/1247886485-1-16.mp4?e=ig8euxZM2rNcNbRVhwdVhwdlhWdVhwdVhoNvNC8BqJIzNbfq9rVEuxTEnE8L5F6VnEsSTx0vkX8fqJeYTj_lta53NCM=&uipk=5&nbs=1&deadline=1693156021&gen=playurlv2&os=bcache&oi=3704020481&trid=0000b26dccdf12744f6595bf84381464d150h&mid=0&platform=html5&upsig=ba1c80a6baca7908fd58211a403eee19&uparams=e,uipk,nbs,deadline,gen,os,oi,trid,mid,platform&cdnid=86604&bvc=vod&nettype=0&f=h_0_0&bw=51611&logo=80000000';
 
   @override
